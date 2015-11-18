52f51460c749225bbc7cd7e1538555e6a43c0793
Emilio Tirado <emilio.tirado57@gmail.com>
2015-11-17 23:32:46 -0430

    adding persistent authentication

--- frontend/src/app/components/login/login.controller.js
+++ frontend/src/app/components/login/login.controller.js
@@ -1,9 +1,33 @@
 'use strict';
 
 angular.module('frontend')
-	.controller('LoginCtrl', ['$scope', 'auth', function($scope, auth){
+	.controller('LoginCtrl', ['$scope', 'auth', '$state', 'store', function($scope, auth, $state, store){
 		$scope.user = '';
 		$scope.password = '';
+
+		function onLoginSuccess(profile, token){
+			$scope.$parent.message = '';
+			$state.go('home');
+			$scope.loading = false;
+			store.set('profile', profile);
+			store.set('token', token);
+		}
+
+		function onLoginFailed(){
+			$scope.$parent.message = 'invalid credentials';
+			$scope.loading = false;
+		}
+
+		$scope.doGoogleAuthWithPopup = function(){
+			$scope.$parent.message = 'loading ...';
+			$scope.loading = true;
+
+			auth.signin({
+				popup : false,
+				connection : 'google-oauth2',
+				scope: 'openid name email'
+			}, onLoginSuccess, onLoginFailed);
+		}
 
 		console.log('login');
 	}]);
--- frontend/src/app/components/login/logout.controller.js
+++ frontend/src/app/components/login/logout.controller.js
@@ -4,5 +4,9 @@ angular.module('frontend')
 	.controller('LogoutCtrl', ['$scope', 'auth', function($scope, auth){
 		console.log('logout');
 
-		
+		auth.signout();
+		store.remove('profile');
+		store.remove('token');
+		$scope.$parent.message = '';
+		$state.go('login');
 	}]);
