cdff63ca471f24080f64791495a351e0d4fc6cba
Emilio Tirado <emilio.tirado57@gmail.com>
2015-11-17 23:42:26 -0430

    testing backend connection

--- frontend/src/app/main/main.controller.js
+++ frontend/src/app/main/main.controller.js
@@ -1,11 +1,15 @@
 'use strict';
 
 angular.module('frontend')
-.controller('MainCtrl', function ($scope, auth) {
-	auth.profilePromise.then(function(){
-		$scope.$parent.message = 'Welcome ' + auth.profile.name + '!';
-		console.log($scope.$parent.message);
-	});
-
-	$scope.auth = auth;
+.controller('MainCtrl', function ($scope, $http) {
+	$scope.callApi = function(){
+		$http({
+			url : 'http://localhost:5000/users',
+			method : 'GET'
+		}).then(function(response){
+			console.log('success', response);
+		}, function(response){
+			console.log('error ', response);
+		});
+	};
 });
