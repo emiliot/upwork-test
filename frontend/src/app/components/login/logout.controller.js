6dec80bc03b6cb07bac8d3063fcc249fb9bb2f48
Emilio Tirado <emilio.tirado57@gmail.com>
2015-11-27 00:47:35 -0430

    final touch

--- frontend/src/app/components/login/logout.controller.js
+++ frontend/src/app/components/login/logout.controller.js
@@ -1,12 +1,9 @@
 'use strict';
 
 angular.module('frontend')
-	.controller('LogoutCtrl', ['$scope', 'auth', function($scope, auth){
-		console.log('logout');
-
+	.controller('LogoutCtrl', ['$scope', 'auth', 'store', '$state',  function($scope, auth, store, $state){
 		auth.signout();
 		store.remove('profile');
 		store.remove('token');
-		$scope.$parent.message = '';
 		$state.go('login');
 	}]);
